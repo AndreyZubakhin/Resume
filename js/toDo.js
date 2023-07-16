6af49a1de0c2fe5098e643e3193f71f46d37b501
AndreyZubakhin <zubakhin.andrey@yandex.ru>
2023-07-16 16:27:24 +0300

    upd

---
+++ js/toDo.js
@@ -0,0 +1,52 @@
+//Необходимо получить элементы со страницы
+
+const form = document.querySelector('#form');
+//console.log(form);
+const taskInput = document.querySelector('#taskInput');
+//console.log(taskInput);
+const tasksList = document.querySelector('#tasksList');
+//console.log(tasksList);
+const emptyList = document.querySelector('#emptyList');
+//console.log(emptyList);
+
+form.addEventListener('submit', addTask);
+
+// Функцию можно вызывать в любое время, даже если функция идет ниже строчки кода в которой она вызывается
+
+function addTask(event) {
+//Метод отменяет поведение по умолчанию. Отменяет обновление страницы при отправке формы, форма не отправляется
+event.preventDefault();
+
+//Получаем текст задачи из поля для ввода
+const taskText = taskInput.value
+//console.log(taskText);
+
+//Формируем разметку для задач
+const taskHTML = `
+<li class="list-group-item d-flex justify-content-between task-item">
+    <span class="task-title">${taskText}</span>
+        <div class="task-item__buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
+        </div>
+</li>
+`;
+//console.log(taskHTML);
+
+//Добавляем задачу на страницу
+tasksList.insertAdjacentHTML('beforeend', taskHTML);
+
+//Очищаем поле для ввода текста и назад возращаем на него фокусировку
+taskInput.value = "";
+taskInput.focus();
+
+//Добавляем проверку, что если в списке задач более 1 элемента, то скрываем блок "Список дел пуст" 
+    if(tasksList.children.length > 1) {
+        emptyList.classList.add('none');
+    }
+}
+
